e907513b4efbc998b7219952bf75236388232d3a
ChurchDuck1 <bchurch2@csus.edu>
2026-06-09 10:30:46 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">
+        Family Dinner Planner
+      </h1>
+
+      <p className="mt-4">
+        First proof of concept.
+      </p>
+    </main>
+  );
+}
